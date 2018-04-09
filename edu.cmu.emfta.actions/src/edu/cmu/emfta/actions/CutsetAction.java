9d109e35eb06ba2435e0f54f4a681af874c75eef
balayyoub <balayyoub@gmail.com>
2018-04-09 13:04:49 -0400

    Update of CutsetAction.java to replace possible illegal symbols in name.

--- edu.cmu.emfta.actions/src/edu/cmu/emfta/actions/CutsetAction.java
+++ edu.cmu.emfta.actions/src/edu/cmu/emfta/actions/CutsetAction.java
@@ -144,7 +144,7 @@ public class CutsetAction extends AbstractExternalJavaAction {
 		if(fileName == null || fileName.trim().length() < 1) {
 			fileName = ResourceUtil.getFile(event.eResource()).getName();
 		}
-		fileName = fileName.replace(".emfta", "") + "-cutset.xlsx";
+		fileName = fileName.replace(".emfta", "").replace("::", "_") + "-cutset.xlsx";
 //		System.out.println("filename=" + fileName);
 
 //		System.out.println(cs);
